6cc29e0437a761ecf132b73f298668ccf81aa8fb
SEUNGDO-LEE <magnipeal@gmail.com>
2025-07-08 07:53:44 +0900

    Update content_loader.py

--- content_loader.py
+++ content_loader.py
@@ -64,57 +64,6 @@ def get_transcript(video_id, lang_list=["ko", "en"]):
         return f"❌ 자막 불러오기 실패: {str(e)}"
     
 
-def copy_to_temp(filepath):
-    filename = os.path.basename(filepath)
-    safe_dir = "tmp_audio"
-
-    if not os.path.exists(safe_dir):
-        os.makedirs(safe_dir)
-
-    safe_path = os.path.join(safe_dir, filename)
-    shutil.copyfile(filepath, safe_path)
-    return safe_path
-
-#KEYWORDS = ["ETF", "리스크", "위험", "변동성", "금융", "파생", "자산운용"]
-def download_audio(youtube_url, idx):
-    filename = f"audio_{idx}.m4a"
-    try:
-        subprocess.run([
-            "yt-dlp",
-            "-f", "bestaudio[ext=m4a]",
-            "-o", filename,
-            youtube_url
-        ], check=True)
-        return filename
-    except subprocess.CalledProcessError:
-        return None
-    
-def transcribe_audio(filepath):
-    transcriber = aai.Transcriber()
-    transcript = transcriber.transcribe(filepath, config=aai.TranscriptionConfig(
-        auto_highlights=True,
-        summarization=True,
-        summary_type="bullets",
-        summary_model="informative"      # 필수 값!
-    ))
-    return transcript.text
-    
-def summarize_text(text, keyword, title):
-    prompt = (
-        f"다음은 유튜브 영상의 전사 내용이야\n"
-        f"전사 내용: {text}\n"
-        f"이 영상 검색에 사용된 키워드는 {keyword}이고 영상의 제목은 {title}이야. 이 내용을 400자 이내로 요약해줘.\n"
-        "가능하다면 사회적·정치적·윤리적 또는 법적 리스크 요소가 있는지도 추정해줘.\n"
-    )   
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[{"role": "user", "content": prompt}]
-    )
-    return response.choices[0].message.content
-
-def clear_tmp_audio():
-    shutil.rmtree("tmp_audio", ignore_errors=True)
-                              
 def fetch_filtered_rss_articles(keyword_list):
     results = []
     for url in RSS_FEEDS:
